6706890ec0f684362d969d3213fa75d10d94515c
Ridzu <44227831+Ridzu95@users.noreply.github.com>
2019-04-02 13:20:37 +0200

    Added contact modal linked with Slack

---
+++ src/components/contactModal/form.js
@@ -0,0 +1,90 @@
+import {
+  Modal, Form, Input, Menu, Button
+} from 'antd'
+import './form.css'
+import React, { Component } from 'react'
+import TextArea from 'antd/lib/input/TextArea'
+import axios from 'axios'
+
+
+const CollectionCreateForm = Form.create({ name: 'form_in_modal' })(
+  // eslint-disable-next-line
+  class extends React.Component {
+    render() {
+      const {
+        visible, onCancel, onCreate, form,
+      } = this.props;
+      const { getFieldDecorator } = form;
+      return (
+        <Modal
+          visible={visible}
+          title="Contacter l'UTT Net Group"
+          okText="Envoyer"
+          cancelText="Annuler"
+          onCancel={onCancel}
+          onOk={onCreate}
+        >
+          <Form layout="vertical">
+            <Form.Item label="Prénom Nom">
+              {getFieldDecorator('name', {
+                rules: [{ required: true, message: 'Veuillez saisir votre prénom et votre nom' }],
+              })(
+                <Input />
+              )}
+            </Form.Item>
+            <Form.Item label="Adresse e-mail">
+              {getFieldDecorator('email', {
+                rules: [{ required: true, message: 'Veuillez saisir une adresse e-mail valide', pattern:  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ }],
+              })(
+                <Input type='email' />
+              )}
+            </Form.Item>
+            <Form.Item label="Message">
+              {getFieldDecorator('message', { rules: [{ required: true, message: 'Veuillez saisir un message' }]})(<TextArea type="textarea" />)}
+            </Form.Item>
+          </Form>
+        </Modal>
+      );
+    }
+  }
+);
+
+class CollectionsPage extends React.Component {
+
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields(async (err, values) => {
+      if (err) {
+        return;
+      }
+      console.log('Received values of form: ', values);
+      let result = await axios.post(
+        process.env.REACT_APP_SLACK_URL,
+        {
+          text : `Nom : ${values.name} \n Adresse mail : ${values.email} \n Message : ${values.message}`
+        },
+
+      );
+      console.log(result);
+      form.resetFields();
+      this.props.handleCancel();
+    });
+  }
+
+  saveFormRef = (formRef) => {
+    this.formRef = formRef;
+  }
+
+  render() {
+    return (
+        <CollectionCreateForm
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.props.visible}
+          onCancel={this.props.handleCancel}
+          onCreate={this.handleCreate}
+        />
+    );
+  }
+}
+
+export default CollectionsPage
